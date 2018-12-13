f98fa38c62e4e3082a0199cb0fe562fe077db91e
duanzengguang <duanzengguang@duanzengguangde.local>
2018-12-13 17:09:43 +0800

    Initial Commit

---
+++ WindingRules/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WindingRules
+//
+//  Created by duanzengguang on 2018/12/13.
+//  Copyright © 2018年 duanzengguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
